1d95c283a5d10e7f1d25943fd1152ca0d3f8772b
mathangan_107177 <jeyakantharajah.mathangan@axiatadigitallabs.com>
2019-10-23 16:37:16 +0530

    Added react navigation and async-hooks

---
+++ App/screens/Home/containers/homeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class HomeContainer extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hom e container is here </Text>
+            </View>
+        )
+    }
+}
+
+export default HomeContainer;
